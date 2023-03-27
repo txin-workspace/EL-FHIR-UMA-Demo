49dc06b3e230017bdc3bb5cccd26e40787bf8b53
t-xin <84426501+txin-workspace@users.noreply.github.com>
2023-03-27 10:32:54 +0900

    Update start_demo.sh

--- start_demo.sh
+++ start_demo.sh
@@ -17,10 +17,10 @@ echo "Make docker compose variable"
 sed "s/YOUR_ADDRESS/${ADDR}/g" ./docker-compose.template > ./docker-compose.yaml
 
 echo "Make postman collections variable"
-# sed "s/YOUR_ADDRESS/${ADDR}/g" ./PostmanCollections/HAPI-FHIR.postman_collection.temp > ./PostmanCollections/HAPI-FHIR.postman_collection.json
-# sed "s/YOUR_ADDRESS/${ADDR}/g" ./PostmanCollections/HAPI-RS.postman_collection.temp > ./PostmanCollections/HAPI-RS.postman_collection.json
-# sed "s/YOUR_ADDRESS/${ADDR}/g" ./PostmanCollections/KeyCloak-API.postman_collection.temp > ./PostmanCollections/KeyCloak-API.postman_collection.json
-# sed "s/YOUR_ADDRESS/${ADDR}/g" ./PostmanCollections/ECHONETLite-RS.postman_collection.temp > ./PostmanCollections/ECHONETLite-RS.postman_collection.json
+sed "s/YOUR_ADDRESS/${ADDR}/g" ./PostmanCollections/HAPI-FHIR.postman_collection.temp > ./PostmanCollections/HAPI-FHIR.postman_collection.json
+sed "s/YOUR_ADDRESS/${ADDR}/g" ./PostmanCollections/HAPI-RS.postman_collection.temp > ./PostmanCollections/HAPI-RS.postman_collection.json
+sed "s/YOUR_ADDRESS/${ADDR}/g" ./PostmanCollections/KeyCloak-API.postman_collection.temp > ./PostmanCollections/KeyCloak-API.postman_collection.json
+sed "s/YOUR_ADDRESS/${ADDR}/g" ./PostmanCollections/ECHONETLite-RS.postman_collection.temp > ./PostmanCollections/ECHONETLite-RS.postman_collection.json
 
 echo "Start docker containers"
 docker compose --verbose up -d --build
