cbc00f582adf360cb1f8417c14a895bc7c33a365
t-xin <84426501+txin-workspace@users.noreply.github.com>
2023-03-27 10:35:04 +0900

    Update end_demo.sh

--- end_demo.sh
+++ end_demo.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
+echo "finish containers"
 docker compose down
 
+echo "delete realms jsons"
 rm ./realm/app_realm.json
 rm ./realm/el_realm.json
 rm ./realm/pcha_realm.json
 
+echo "delete compose yaml"
 rm ./docker-compose.yaml
+
+echo "delete postman jsons"
+rm ./PostmanCollections/HAPI-FHIR.postman_collection.json
+rm ./PostmanCollections/HAPI-RS.postman_collection.json
+rm ./PostmanCollections/KeyCloak-API.postman_collection.json
+rm ./PostmanCollections/ECHONETLite-RS.postman_collection.json
+
+echo "finished"
